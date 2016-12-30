7a486adac93c16c6be0e2c5c20ab86d67075445e
Ryan Morris <ryanmorris.j@gmail.com>
2016-12-30 08:44:31 -0800

    Setup basic react structure

---
+++ web/static/js/public/index.js
@@ -0,0 +1,8 @@
+import ReactDOM from 'react-dom';
+import React, { Component } from 'react';
+
+import App from "./public/components/app";
+
+ReactDOM.render(
+  <App />, document.getElementById("react-app")
+)
